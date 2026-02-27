cd375e6f4875909501c0059ccd95542cdbb1a348
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2026-02-27 19:26:21 +0900

    chore: try r2

--- examples/basic/src/framework/use-cache.ts
+++ examples/basic/src/framework/use-cache.ts
@@ -1,12 +1,17 @@
 import { env } from "cloudflare:workers";
 import { createStorage } from "unstorage";
-import cloudflareKVBindingDriver from "unstorage/drivers/cloudflare-kv-binding";
+import cloudflareR2BindingDriver from "unstorage/drivers/cloudflare-r2-binding";
+// import cloudflareKVBindingDriver from "unstorage/drivers/cloudflare-kv-binding";
 import { provideCache } from "vite-plugin-react-use-cache/runtime";
+// // https://unstorage.unjs.io/drivers/cloudflare#cloudflare-kv-binding
+// const storage = createStorage({
+//   driver: cloudflareKVBindingDriver({ binding: env.KV }),
+// });
 import { createUnstorageCache } from "vite-plugin-react-use-cache/unstorage";
 
-// https://unstorage.unjs.io/drivers/cloudflare#cloudflare-kv-binding
+// https://unstorage.unjs.io/drivers/cloudflare#cloudflare-r2-binding
 const storage = createStorage({
-  driver: cloudflareKVBindingDriver({ binding: env.KV }),
+  driver: cloudflareR2BindingDriver({ binding: env.R2 }),
 });
 
 export function withUseCache<T>(handler: () => Promise<T>): Promise<T> {
